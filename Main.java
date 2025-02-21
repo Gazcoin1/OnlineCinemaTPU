ecf797b99663a0bc868b485581b57258304950e2
gazco <gazcoin-1@bk.ru>
2025-02-22 02:30:51 +0700

    fdg

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String s = "https://github.com/Gazcoin1/OnlineCinemaTPU";
+        System.out.println(s.substring(19).split("/")[0]);
+        System.out.println(s.substring(19).split("/")[1]);
+    }
+}
